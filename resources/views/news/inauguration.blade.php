bdc21390dc9aec3e54c70adffc54545f980cb121
12345abuja <christyseeker@gmail.com>
2020-12-04 15:27:51 +0100

    ade

--- resources/views/news/inauguration.blade.php
+++ resources/views/news/inauguration.blade.php
@@ -5,7 +5,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
-                <img src="/image/inau2.jpg" alt="" class="w-100">
+                <img src="/image/news2.jpg" alt="" class="w-100">
                 <div class="w-100 bg-success p-3"></div>
             </div>
             <div class="col-12 mb-1 text-center">
@@ -13,17 +13,65 @@
             </div>
         </div>
         <div class="container">
-            <div class="row justify-content-center p-2">
+            <div class="row justify-content-center m-2 bg-white py-2">
                 <div class="col-md-6 pr-3">
                     <p>
                         Al-Ansar Foundation (AAF) in its efforts to fulfill its vision and mission of providing services to humanity has established a Radio and TV Station known as Al-Ansar Radio and TV. The radio station which is currently adjudged to be one of the best radio stations in the State in terms of state-of-the-art facilities, when fully operational would run on the modulation of 96.1 FM.
                     </p>
                     <p>
-                        The Al-Ansar Radio and TV station is owned by Al-Ansar Foundation - a Humanitarian Support Organization established in 2015 to provide services to humanity through quality education; healthcare and humanitarian services to vulnerable and less-privileged children, youths and women in Nigeria especially in the North-east region of the country.
+                        The Al-Ansar Radio and TV station is owned by Al-Ansar Foundation - a Humanitarian Support Organization established in 2015 to provide services to humanity through the provision of quality education; healthcare and humanitarian services to vulnerable and less-privileged children, youths and women in Nigeria especially in the North-east region of the country.
                     </p>
                 </div>
                 <div class="col-md-6">
-                    <img src="/image/" alt="" class="w-100 d-block">
+                    <img src="/image/inau2.jpg" alt="" class="w-100 d-block">
+                </div>
+            </div>
+            <div class="row justify-content-center m-2 bg-white py-2">
+                <div class="col-md-6">
+                    <img src="/image/inau.jpg" alt="" class="w-100 d-block">
+                </div>
+                <div class="col-md-6 pr-3">
+                    <p>
+                        Public Financial Management Maestro and Seasoned Technocrat, Dr. M. K. Dikwa, mni, Ph.D. FCNA, ACA, FCTI on Saturday 21st, November, 2020 inaugurated the Board of Al-Ansar Radio and TV station, Al-Ansar Radio 91.6 FM in Abuja, the Federal Capital Territory (FCT).
+                    </p>
+                    <p>
+                        Dr. Dikwa, while inaugurating the Board said that “the inauguration of the Board became imperative given the fact that the Al-Ansar Radio and TV is at the verge of commencing full operations as all certifications from regulatory authorities especially from the National Broadcasting Commission (NBC) has been secured.”
+                    </p>
+                    <p>
+                        In his inaugural address, the Public Finance Management maestro used the occasion to emphasis the vision behind the establishment of the Al-Ansar Radio and TV station. According to him: “Our vision is clear and that is to amongst others things:
+                    </p>
+                </div>
+            </div>
+            <div class="row justify-content-center m-2 bg-white py-2">
+                <div class="col-md-6 pr-3">
+                    <p>
+                        <ol type="I">
+                            <li>Support the government’s efforts towards bringing lasting peace, tranquility and development to our dear state – Borno State.</li>
+                            <li>Build peace and harmony among the societies in our dear state in particular and the North-east region in particular by airing deliberate programmes that supports peace building;</li>
+                            <li>Publicize government developmental initiatives in the state; </li>
+                            <li>Propagate programmes that would promote diversification of the nation’s economy such as agriculture, solid minerals, tourism, hospitality etc; and</li>
+                            <li>Operate strictly within the confines of the National Broadcasting Commission (NBC)’s ACT.”</li>
+                        </ol>
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <img src="/image/inau3.jpg" alt="" class="w-100 d-block">
+                </div>
+            </div>
+            <div class="row justify-content-center m-2 bg-white py-2">
+                <div class="col-md-5">
+                    <img src="/image/dikwa.jpg" alt="" class="w-100 d-block">
+                </div>
+                <div class="col-md-7 pr-3">
+                    <p>
+                        <ol type="I">
+                            <li>Support the government’s efforts towards bringing lasting peace, tranquility and development to our dear state – Borno State.</li>
+                            <li>Build peace and harmony among the societies in our dear state in particular and the North-east region in particular by airing deliberate programmes that supports peace building;</li>
+                            <li>Publicize government developmental initiatives in the state; </li>
+                            <li>Propagate programmes that would promote diversification of the nation’s economy such as agriculture, solid minerals, tourism, hospitality etc; and</li>
+                            <li>Operate strictly within the confines of the National Broadcasting Commission (NBC)’s ACT.”</li>
+                        </ol>
+                    </p>
                 </div>
             </div>
         </div>
